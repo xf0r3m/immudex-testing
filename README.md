65867cd0172f4e0d025bf0dc5c24162c1da2e641
xf0r3m <jakubstasinski@protonmail.com>
2022-11-22 18:33:24 +0100

    Przesłanie plików na repozytorium

---
+++ README.md
@@ -0,0 +1,48 @@
+![Image](https://i.ibb.co/NxtyJ3T/immudex2.png)
+
+# IMMutable DEbian with Xfce - Testing
+
+## GNU/LINUX Debian testing (bookworm)
+
+Immudex to wersja GNU/Linux Debian zawierająca niezmienne środowisko pracy. Wykorzystuje
+ona bowiem archiwum .squashfs znane z LiveCD. Przyczym pozwala ona na pełen
+dostęp do partycji zawierające archiwum, w razie aktualizacji. Tak przygotowana
+wersja popularnego systemu operacyjnego pozwoli bezpieczniejsze korzystanie
+z komputera oraz zasobów internetu. Jeśli coś się stanie, wystarczy uruchomić
+komputer ponownie.
+
+Immudex nastawiowny jest na wykorzystanie do przechowywania danych szyfrowanych
+partycji za pomocą mechanizmu LUKS. Dodatkowy mechanizm pozwala na 
+zabezpieczenie danych składowanych na tego typu partycjach, na przykład jeśli
+mamy otwarte jakieś pliki na zaszyfrowanej partycji przeglądarka się nie
+uruchomi. Musimy wówczas zamknąć wszystkie pliki oraz opuścić punkt montowania
+szyfrowanej partycji, wtenczas zostanie ona odmontowana szyfrowany wolumin
+zostanie zamknięty. Po tych czynnościach przeglądarką uruchomi się
+samodzielnie. W ramach bezpieczeństwa dostępny jest również sandboxer FireJail,
+zablokowano również wszelkie sieciowe połączenia przychodzące.
+
+Immudex domyślnie korzysta z wolnego oprogramowania, nie zainstalowano na nim
+niewolnych pakietów, w konfiguracji nie ma również niewolnych repozytoriów.
+Do dyspozycji mamy:
+  * Standardowe środowisko XFCE dostępne na Debian Stable
+  * Odtwarzacz muzyki QMMP
+  * Odtwarzacz multimedialny VLC
+  * virt-manager (KVM)
+
+Obraz płyty znajduje się na serwisie sourceforge.net. Poniżej znajduje się 
+odnośniki.
+  * 64-bit: [https://sourceforge.net/projects/immudex/files/iso/0.0.6/immudex64.iso/download](https://sourceforge.net/projects/immudex/files/iso/0.0.6/immudex64.iso/download)
+    CRC: 3859012829 SHA1: 4aafe83073eb46d2fb325dcae44422e787805187
+  * 32-bit: [https://sourceforge.net/projects/immudex/files/iso/0.0.6/immudex32.iso/download](https://sourceforge.net/projects/immudex/files/iso/0.0.6/immudex32.iso/download)
+    CRC: 325219353 SHA1: 63d29e054a95ac343354ead25cca3e9f538cf125
+
+Domyślnym użytkownikiem jest user, dostęp to niego uzyskujemy za pomocą hasła
+user1. Możemy również skorzystać z konta superużytkownika root z hasłem toor.
+
+immudex is not affiliated with Debian. Debian is a registered trademark owned 
+by Software in the Public Interest, Inc.
+
+[Lock icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/lock)
+
+[Rss icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/rss)
+ 
---
+++ versions/base.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+set -e;
+
+if [ "$1" = "64" ]; then
+  apt install --no-install-recommends linux-image-amd64 live-boot systemd-sysv -y;
+else
+  apt install --no-install-recommends linux-image-686-pae live-boot systemd-sysv -y;
+fi
+
+apt install -y tzdata locales keyboard-configuration console-setup
+dpkg-reconfigure tzdata;
+dpkg-reconfigure locales;
+dpkg-reconfigure keyboard-configuration;
+dpkg-reconfigure console-setup;
+
+apt install -y task-desktop task-xfce-desktop;
+
+apt install -y git firejail ufw cryptsetup lsof extlinux grub-efi-amd64 efibootmgr bash-completion etherwake wakeonlan cifs-utils wget figlet chirp mpv youtube-dl vim-gtk3 redshiftlibssl-dev libbz2-dev libnss3-dev libgdbm-dev libncurses5-dev libffi-dev libreadline-dev libsqlite3-dev zlib1g-dev build-essential;
+
+apt install --no-install-recommends software-properties-common -y;
+
+wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tar.xz -O Python-3.11.0.tar.xz;
+tar -xvf Python-3.11.0.tar.xz;
+cd Python-3.11.0;
+
+./configure --prefix=/usr/local --enable-optimizations
+sudo make
+sudo altinstall
+
+cp /usr/bin/youtube-dl /usr/bin/youtube-dl-orig;
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/bin/youtube-dl;
+chmod +x /usr/bin/youtube-dl;
+
+cd;
+git clone https://github.com/xf0r3m/xfcedebian.git;
+cd xfcedebian;
+chmod +x install.sh;
+bash install.sh;
+
+cd;
+git clone https://github.com/xf0r3m/immudex.git;
+cd immudex;
+
+tar -xzvf mozilla.tgz -C /etc/skel;
+chown -R root:root /etc/skel/.mozilla;
+
+cp -vv tools/* /usr/local/bin;
+#cp tools/protected /usr/local/bin;
+chmod +x /usr/local/bin/*;
+
+cp -v launchers/16608166085.desktop /etc/skel/.config/xfce4/panel/launcher-19/
+cp -v launchers/16674118881.desktop /etc/skel/.config/xfce4/panel/launcher-32/
+rm /etc/skel/.config/xfce4/panel/launcher-32/16608177609.desktop
+cp -rv launchers/launcher-29 /etc/skel/.config/xfce4/panel;
+cp -v files/xfce4-panel.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml;
+
+#sed -i '26s/1/0' /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml;
+#sed -i '64i\\\t<property name="disable-struts" type=bool value="false"/>' /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml;
+
+cp -v images/padlock.png /usr/share/images/desktop-base;
+cp -v images/unlocked.png /usr/share/images/desktop-base;
+cp -v images/rss.png  /usr/share/icons;
+cp -v files/redshift.conf /etc/skel/.config;
+mkdir -v /etc/skel/.config/autostart;
+cp -vv files/redshift.desktop /etc/skel/.config/autostart;
+
+#echo "/usr/local/bin/motd2" >> /etc/bash.bashrc;
+
+echo "if [ ! -f /tmp/.motd ]; then" >> /etc/bash.bashrc;
+echo "  /usr/local/bin/motd2;" >> /etc/bash.bashrc;
+echo "  touch /tmp/.motd;" >> /etc/bash.bashrc;
+echo "fi;" >> /etc/bash.bashrc;
+
+echo "alias chhome='export HOME=\$(pwd)'" >> /etc/bash.bashrc;
+echo "alias ytstream='mpv --ytdl-format=best[height=480]'" >> /etc/bash.bashrc;
+
+chmod u+s /usr/bin/ping;
+sed -i -e 's/chirpw/sudo chirpw/' -e 's/false/true' /usr/share/applications/chirp.desktop;
+
+ufw default deny incoming;
+ufw default allow outgoing;
+ufw enable;
+useradd -m -s /bin/bash xf0r3m;
+echo "xf0r3m:xf0r3m1" | chpasswd;
+useradd -m -s /bin/bash user;
+echo "user:user1" | chpasswd;
+echo "xf0r3m ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers;
+echo "user ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers;
+echo "root:toor" | chpasswd;
+usermod -aG libvirt,libvirt-qemu xf0r3m;
+usermod -aG libvirt,libvirt-qemu user;
+echo "immudex" > /etc/hostname;
+echo "127.0.1.1    immudex" >> /etc/hosts;
+echo "deb http://ftp.icm.edu.pl/pub/Linux/debian/ bullseye main" > /etc/apt/sources.list;
+echo "deb-src http://ftp.icm.edu.pl/pub/Linux/debian/ bullseye main" >> /etc/apt/sources.list;
+echo "deb http://security.debian.org/debian-security bullseye-security main" >> /etc/apt/sources.list;
+echo "deb-src http://security.debian.org/debian-security bullseye-security main" >> /etc/apt/sources.list;
+echo "deb http://ftp.icm.edu.pl/pub/Linux/debian/ bullseye-updates main" >> /etc/apt/sources.list;
+echo "deb-src http://ftp.icm.edu.pl/pub/Linux/debian/ bullseye-updates main" >> /etc/apt/sources.list;
+apt update;
+apt upgrade -y;
+cd;
+rm -rf immudex/
+rm -rf xfcedebian/
+apt-get clean;
+apt-get clean;
+apt-get autoremove -y;
+apt-get autoclean;
+echo > ~/.bash_history
+history -c
