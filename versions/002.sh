7a38fbd8af4195ebe63de9bb5ba0035a6e79e1ec
xf0r3m <jakubstasinski@protonmail.com>
2022-12-24 15:04:51 +0100

    Zmiana w pliku wersji 0.0.2

--- versions/002.sh
+++ versions/002.sh
@@ -6,6 +6,31 @@ if [ ! "$version" ]; then echo -e "\e[31mUpdate failed!\e[0m"; exit 1; fi;
 apt update
 apt upgrade -y
 
+sed -i '26s/1/0/' /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml;
+sed -i '64i\\t<property name="disable-struts" type="bool" value="false"/>' /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml;
+
+userdel -r user;
+userdel -r xf0r3m;
+
+useradd -m -s /bin/bash user;
+if [ ! -f /home/user/.vimrc ]; then
+  cp -rvv /etc/skel/.??* /home/user;
+  chown -R user:user /home/user;
+fi
+echo "user:user1" | chpasswd;
+
+useradd -m -s /bin/bash xf0r3m;
+if [ ! -f /home/xf0r3m/.vimrc ]; then
+  cp -rvv /etc/skel/.??* /home/xf0r3m;
+  chown -R xf0r3m:xf0r3m /home/xf0r3m;
+fi
+echo "xf0r3m:xf0r3m1" | chpasswd;
+
+echo "root:toor" | chpasswd;
+
+usermod -aG libvirt,libvirt-qemu xf0r3m;
+usermod -aG libvirt,libvirt-qemu user;
+
 apt-get clean;
 apt-get clean;
 apt-get autoremove -y;
