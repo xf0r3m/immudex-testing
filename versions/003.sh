5ca52b56806d2fedf0a0d2d869f34e6314942245
xf0r3m <jakubstasinski@protonmail.com>
2022-12-31 12:27:16 +0100

    Przygotowanie do wydania immudex-testing 0.0.3 - część 2

--- versions/003.sh
+++ versions/003.sh
@@ -14,6 +14,28 @@ fi
 cp -vv ~/immudex-testing/tools/${version}/* /usr/local/bin;
 chmod +x /usr/local/bin/*;
 
+cp -vv ~/immudex-testing/files/xfce4-panel.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml;
+
+userdel -r user;
+userdel -r xf0r3m;
+
+useradd -m -s /bin/bash user;
+if [ ! -f /home/user/.vimrc ]; then
+  cp -rvv /etc/skel/.??* /home/user;
+  chown -R user:user /home/user;
+fi
+echo "user:user1" | chpasswd;
+
+useradd -m -s /bin/bash xf0r3m;
+if [ ! -f /home/xf0r3m/.vimrc ]; then
+  cp -rvv /etc/skel/.??* /home/xf0r3m;
+  chown -R xf0r3m:xf0r3m /home/xf0r3m;
+fi
+echo "xf0r3m:xf0r3m1" | chpasswd;
+
+usermod -aG libvirt,libvirt-qemu xf0r3m;
+usermod -aG libvirt,libvirt-qemu user;
+
 apt-get clean;
 apt-get clean;
 apt-get autoremove -y;
