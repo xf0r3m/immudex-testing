9019032cc612da3e21471d54ce4b125220be1931
xf0r3m <jakubstasinski@protonmail.com>
2023-06-03 19:06:05 +0200

    Przygotowanie do wydania immudex-testing 0.2.5 - część 3

--- versions/025.sh
+++ versions/025.sh
@@ -9,5 +9,13 @@ source ~/immudex-testing/versions/template.sh;
 
 update_packages;
 
+cp -vv ~/immudex-testing/tools/${VERSION}/immudex_crypt /usr/local/bin;
+cp -vv ~/immudex-testing/tools/${VERSION}/immudex_hostname /usr/local/bin;
+cp -vv ~/immudex-testing/tools/${VERSION}/meteo /usr/local/bin;
+cp -vv ~/immudex-testing/tools/${VERSION}/ytplay /usr/local/bin;
+
+cp -vv ~/immudex-testing/files/${VERSION}/immudex_hostname.service /etc/systemd/system;
+systemctl enable immudex_hostname.service;
+
 echo "root:toor" | chpasswd;
 tidy;
