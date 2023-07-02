034f8b5df41bc2ac446d34fc5b04392aa72d8743
xf0r3m <jakubstasinski@protonmail.com>
2023-07-02 07:28:21 +0200

    Przygotowanie do wydania immudex-testing 1.0.2 - część 1

---
+++ versions/102.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dhclient;
+cd
+if [ -x /usr/bin/git ]; then git clone https://github.com/xf0r3m/immudex-testing.git;
+else apt install git && git clone https://github.com/xf0r3m/immudex-testing.git;
+fi
+source ~/immudex-testing/versions/template.sh;
+
+update_packages;
+
+set_mime;
+
+tidy;
+
+rm $0;
