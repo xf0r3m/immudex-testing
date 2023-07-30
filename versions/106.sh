3166ac138cc7da2429857f8d2437cee1de7a2e03
xf0r3m <jakubstasinski@protonmail.com>
2023-07-30 09:05:04 +0200

    Przygotowanie do wydania immudex-testing 1.0.6 - część 3

--- versions/106.sh
+++ versions/106.sh
@@ -12,6 +12,10 @@ update_packages;
 cp -vv ~/immudex-testing/tools/${VERSION}/create_media /usr/local/bin;
 cp -vv ~/immudex-testing/tools/${VERSION}/immudex_hostname /usr/local/bin;
 
+rm -rf /etc/skel/.mozilla;
+tar -xzvf ~/immudex-testing/files/${VERSION}/mozilla.tgz -C /etc/skel;
+recreate_users;
+
 set_mime;
 
 tidy;
