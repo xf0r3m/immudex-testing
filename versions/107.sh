5d8d2b6e5758554af26dcc5882ee8497665996d2
xf0r3m <jakubstasinski@protonmail.com>
2023-08-06 07:18:55 +0200

    Przygotowanie do wydania immudex-testing 1.0.7 - część 2

--- versions/107.sh
+++ versions/107.sh
@@ -8,8 +8,12 @@ fi
 source ~/immudex-testing/versions/template.sh;
 
 update_packages;
+apt purge -y xpra;
 
-cp -vv ~/immudex-testing/files/${VERSION}/gtk-main.css /usr/share/xfce4-notes-plugin/gtk-3.0/
+cp -vv ~/immudex-testing/files/${VERSION}/gtk-main.css /usr/share/xfce4-notes-plugin/gtk-3.0;
+cp -vv ~/immudex-testing/files/${VERSION}/xfce4-keyboard-shortcuts.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml;
+
+recreate_users;
 
 set_mime;
 
