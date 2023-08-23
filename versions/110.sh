fa4965b9c3d22c8d3cfee4611423a1990081c7aa
xf0r3m <jakubstasinski@protonmail.com>
2023-08-23 17:28:17 +0200

    Przygotowanie do wydania immudex-testing 1.1.0 - część 1

---
+++ versions/110.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dhclient;
+cd
+if [ -x /usr/bin/git ]; then git clone https://github.com/xf0r3m/immudex-testing.git;
+else apt install git && git clone https://github.com/xf0r3m/immudex-testing.git;
+fi
+source ~/immudex-testing/versions/template.sh;
+
+update_packages;
+
+if [ $(uname -m) = "x86_64" ]; then
+  wget https://ftp.morketsmerke.org/immudex/testing/software/librewolf/librewolf-116.0.3-1.en-US.linux-x86_64.tar.bz2;
+  tar -xf librewolf-116.0.3-1.en-US.linux-x86_64.tar.bz2 -C /usr/lib;
+else
+  wget https://ftp.morketsmerke.org/immudex/testing/software/librewolf/librewolf-116.0.3-1.en-US.linux-i686.tar.bz2;
+  tar -xf librewolf-116.0.3-1.en-US.linux-i686.tar.bz2 -c /usr/lib;
+fi
+ln -s /usr/lib/librewolf/librewolf /usr/bin/librewolf;
+
+cp -vv ~/immudex-testing/launchers/${VERSION}/16844254192.desktop /etc/skel/.config/xfce4/panel/launcher-5;
+cp -vv ~/immudex-testing/files/${VERSION}/librewolf.desktop /usr/share/applications;
+cp -vv ~/immudex-testing/tools/${VERSION}/secured-firefox /usr/local/bin;
+
+tar -xf ~/immudex-testing/files/${VERSION}/librewolf.tgz -C /etc/skel;
+recreate_users;
+
+set_mime;
+
+tidy;
